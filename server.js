70f3d4a8563003a1a61ecac070571ccc11b68c93
Scott-Zeta <wc070211@gmail.com>
2022-10-19 18:34:19 +1030

    change the default port to 3001

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 require('./config/db');
 
 const app = require('express')();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 const UserRouter = require('./api/User');
 
